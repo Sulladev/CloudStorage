115a5a8f6fb207ec3e27cfaffce5f0efbf93e635
sulla <inikolazzz@gmail.com>
2020-10-12 17:35:48 +0300

    authorization

---
+++ geek-cloud-common/src/main/java/common/AuthorizationMessage.java
@@ -0,0 +1,13 @@
+package common;
+
+public class AuthorizationMessage extends AbstractMessage {
+    private String command;
+
+    public AuthorizationMessage(String command) {
+        this.command = command;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+}
